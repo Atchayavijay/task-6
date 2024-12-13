0aa6d1aae6638ec93d0c7c0bad6a3ec3738c3416
Atchayavijay <atchuvijay83@gmail.com>
2024-12-13 18:29:39 +0530

    Add files via upload

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Button.css";
+const Button = ({ num, click }) => {
+  return (
+    <button className="ui-change-btn" onClick={() => click(true)}>
+      Added Cart<span>{num}</span> {num <= 1 ? "item" : "items"}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/components/cards/Card.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import "./Card.css";
+
+import { useState } from "react";
+const Card = ({ product, addItem, removeItem, addedItems }) => {
+  const [isAdded, setIsAdded] = useState(true);
+  const item = addedItems.filter((addedItem) => addedItem.id == product.id);
+  useEffect(() => {
+    item.length == 0 ? setIsAdded(true) : setIsAdded(false);
+  }, [item]);
+
+  // console.log(item);
+  return (
+    <div className="card">
+      <img className="card__img" src={product.image} alt="" />
+      <div>
+        <h2>{product.category}</h2>
+        <h4>{product.title}</h4>
+        <p>{product.description}</p>
+        <h2>10% Offer</h2>
+      </div>
+      <div className="card-price-add">
+        <span>Price : ${product.price}</span>
+        <button
+          className={isAdded ? "add-item-btn" : "remove-item-btn"}
+          onClick={() => {
+            isAdded ? addItem(product) : removeItem(product);
+            setIsAdded(!isAdded);
+          }}
+        >
+          {isAdded ? "ADD To Cart" : "Remove From Cart"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
